568f5a2cb9d570226a951e78cbf5df93499d3df4
Yuji Sode <yuji.sode@gmail.com>
2017-01-18 01:33:26 +0900

    Update canvasPenJS.js

--- canvasPenJS.js
+++ canvasPenJS.js
@@ -1,9 +1,5 @@
 /*canvasPenJS.js
-* ======
-* some code samples for "Toutch events" were derived on 2017-01-16
-* and from "Touch events" by 2005-2017 Mozilla Developer Network (MDN)
-* and individual contributors.
-* ======
+*
 *    Copyright (c) 2016 Yuji SODE <yuji.sode@gmail.com>
 *
 *    This software is released under the MIT License.
@@ -25,7 +21,7 @@
 * function that removes the set drawing/plotting interface
 * and returns log object: {time:string,d:[]}.
 */
-function _canvasPenJS_click(canvas,rgba,w,plot){
+function _canvasPenJS(canvas,rgba,w,plot){
   //============================================================================
   var slf=window,cvs=slf.document.getElementById(canvas.id),I=0,n=0,c,x=0,y=0,Rect,
       log={time:0,d:[]},
@@ -41,54 +37,42 @@ function _canvasPenJS_click(canvas,rgba,w,plot){
   * https://developer.mozilla.org/en/docs/Web/API/Element/getBoundingClientRect
   */
   //============================================================================
-  //== <Handling clicks with touch event: code samples from MDN> ==
-  /* these code samples were derived on 2017-01-16 and from:
-  * "Additional tips/Handling clicks" in "Touch events"
-  * by 2005-2017 Mozilla Developer Network (MDN) and individual contributors:
-  * https://developer.mozilla.org/en/docs/Web/API/Touch_events
-  * the contributors to "Touch events":
-  * chrisdavidmills, andrew-luhring, Luke314, teoli, cassidoo, mrenty, AFBarstow,
-  * Sebastianz, ammist, TotalAMD, mnquintana, mkato, Sheppy, rschmidmeister, Changbin,
-  * maynarddemmon, Dotcom, Thomas-Brierley, jdawson, Jeremie, kscarfone, timothykim,
-  * kohei.yoshino, Gawi, Darbicus, robstar, avih, complynx, Brainversation, ethertank,
-  * foolip, Nickolay, MykMelez, GrapWhit3, openjck,arleytriana, freejosh, smeranda,
-  * julienw, oncletom, wesj, yahelc, MattBrubeck, jswisher
-  * ------
-  * Any copyright of These code samples from MDN is dedicated to the Public Domain.
-  * http://creativecommons.org/publicdomain/zero/1.0/
-  * ------
-  */
-function onTouch(evt) {
-  evt.preventDefault();
-  if (evt.touches.length > 1 || (evt.type == "touchend" && evt.touches.length > 0))
-    return;
-
-  var newEvt = document.createEvent("MouseEvents");
-  var type = null;
-  var touch = null;
-
-  switch (evt.type) {
-    case "touchstart": 
-      type = "mousedown";
-      touch = evt.changedTouches[0];
-      break;
-    case "touchmove":
-      type = "mousemove";
-      touch = evt.changedTouches[0];
-      break;
-    case "touchend":        
-      type = "mouseup";
-      touch = evt.changedTouches[0];
-      break;
-  }
-
-  newEvt.initMouseEvent(type, true, true, evt.originalTarget.ownerDocument.defaultView, 0,
-    touch.screenX, touch.screenY, touch.clientX, touch.clientY,
-    evt.ctrlKey, evt.altKey, evt.shiftKey, evt.metaKey, 0, null);
-  evt.originalTarget.dispatchEvent(newEvt);
+  //== <Handling clicks with touch event> ==
+//this function simulates mouse event via touch event.
+function touch2MouseEvt(e){
+  //e is event object
+  e.preventDefault();
+  if(!!e.changedTouches&&e.changedTouches.length>0){
+    var touch0=null,
+        /*function simulates new mouse event*/
+        newMEvt=function(EventName,tObj,tgt){
+          //tObj and tgt are touch object and target element
+          var E=new MouseEvent(EventName,{
+            'view':window,
+            'bubbles':true,
+            'cancelable':true,
+            'clientX':tObj.clientX,
+            'clientY':tObj.clientY
+          });
+          tgt.dispatchEvent(E);
+        };
+    switch(e.type){
+      case 'touchstart':
+        touch0=e.changedTouches[0];
+        newMEvt('mousedown',touch0,e.target);
+        break;
+      case 'touchmove':
+        touch0=e.changedTouches[0];
+        newMEvt('mousemove',touch0,e.target);
+        break;
+      case 'touchend':
+        touch0=e.changedTouches[0];
+        newMEvt('mouseup',touch0,e.target);
+        break;
+    }
+  }else{return;}
 }
-  //== </Handling clicks with touch event: code samples from MDN> ==
-
+  //== </Handling clicks with touch event> ==
   if(!plot){
     //drawing
     var dr=function(e){
@@ -140,7 +124,7 @@ function onTouch(evt) {
   //============================================================================
   //==
   cvs=slf.document.getElementById(canvas.id),n=evnt[2].length,I=0;
-  while(I<n){cvs.addEventListener(evnt[2][I],onTouch,true),I+=1;}
+  while(I<n){cvs.addEventListener(evnt[2][I],touch2MouseEvt,true),I+=1;}
   //==
   if(!plot){
     //drawing
@@ -160,7 +144,6 @@ function onTouch(evt) {
     };
   }else{
     //plotting
-    //n=evnt[1].length,I=0;
     log.d=[],log.time='plotting:'+slf.Date().replace(/\s/g,'_')+' to ';
     cvs.addEventListener(evnt[1][0],plt,true);
     //returned function
@@ -178,7 +161,7 @@ function onTouch(evt) {
 }
 //=== test examples ===
 //var d=window.document.getElementsByTagName('canvas')[0];
-//var Y=_canvasPenJS_click(d,'rgba(255,0,0,1)',2,false);
+//var Y=_canvasPenJS(d,'rgba(255,0,0,1)',2,false);
 //Y();
-//var Y=_canvasPenJS_click(d,'rgba(0,0,255,1)',10,true);
+//var Y=_canvasPenJS(d,'rgba(0,0,255,1)',10,true);
 //Y();
