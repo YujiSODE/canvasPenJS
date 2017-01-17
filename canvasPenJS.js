4babb4685b46c26763008ec4a9ed1300f60bbb2a
Yuji Sode <yuji.sode@gmail.com>
2017-01-18 01:35:40 +0900

    Update canvasPenJS.js

--- canvasPenJS.js
+++ canvasPenJS.js
@@ -14,7 +14,6 @@
 <ToDo>
 * - plot can be a text: csv formatted pairs of values with @.
 * - a pair of values are expressed as x@y e.g., '1@2,10.5@100.31'.
-- TouchEvent
 - minimum version
 </ToDo>
 * === returned value ===
